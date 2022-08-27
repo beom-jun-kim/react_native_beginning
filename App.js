7392064862b2caabd448159a03250f6594144eff
kimbeomjun <gimb52043@gmail.com>
2022-08-27 23:14:45 +0900

    react_native

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+
+const SEREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN APP!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>daegu</Text>
+      </View>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,12 +39,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#81c147",
   },
-  text: {
-    fontSize: 28,
-    color:'red',
+  city: {
+    flex: 1.2,
+    // backgroundColor: "#81c147",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "600",
+    color: "#eee",
+  },
+  weather: {},
+  day: {
+    width: SEREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 180,
+    color: "#eee",
+  },
+  desc: {
+    fontSize: 50,
+    marginTop: -30,
+    color: "#eee",
   },
 });
